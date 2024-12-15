9032eb8fb9fa3a1b9c999eaa67bb4ed861dc3a92
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-15 19:42:01 +0700

    commit 11 pembuatan data class HomeUiState

--- app/src/main/java/com/example/database/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/database/ui/viewmodel/HomeMhsViewModel.kt
@@ -46,3 +46,9 @@ class HomeMhsViewModel (
             )
         )
 }
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
