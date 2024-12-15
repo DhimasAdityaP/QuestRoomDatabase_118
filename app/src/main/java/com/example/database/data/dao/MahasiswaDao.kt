a1be78f9d371c5af06d87169802590b27d89dd70
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-15 19:19:10 +0700

    commit 2 update MahasiswaDao query getmahasiswa,deletemahasiswa,updatemahasiswa

--- app/src/main/java/com/example/database/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/database/data/dao/MahasiswaDao.kt
@@ -1,17 +1,29 @@
-package com.example.database.data.dao
+package com.pam.questroomdatabase_139.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.database.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
-    @Query("select*from mahasiswa")
-    fun getALLMahasiswa() :  Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+    //deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+    //updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
